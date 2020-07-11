2608ad3875fdc1275a4a8c9693974d9c6cac6ed8
Peyton Plaisance <pplaisance@theharbor.church>
2020-07-11 13:50:57 -0500

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -86,7 +86,7 @@ function sshConnect() {
             // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
             host: '54.173.181.11',
             username: 'ubuntu',
-            privateKey: 'riot-key.pem'
+            privateKey: 'hs-key.pem'
         })
         .then(function () {
             console.log('SSH Connection established.');
